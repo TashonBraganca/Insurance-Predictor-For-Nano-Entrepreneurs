bfae13c8ffcc386f3c6bb54bcae612390e295b94
TashonBraganza <www.tashonbraganza9@gmail.com>
2024-12-20 16:31:00 +0530

    Commits

---
+++ train_model.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import pickle
+
+# Load the dataset
+data = pd.read_csv('insurance.csv')
+
+# Preprocess data
+data['sex'] = data['sex'].map({'male': 0, 'female': 1})
+data['smoker'] = data['smoker'].map({'yes': 1, 'no': 0})
+data = pd.get_dummies(data, columns=['region'], drop_first=True)
+
+# Define features and target
+X = data.drop('charges', axis=1)
+y = data['charges']
+
+# Split the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train the model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Save the trained model to a pickle file
+with open('insurance_model.pkl', 'wb') as file:
+    pickle.dump(model, file)
+
+print("Model trained and saved as insurance_model.pkl")
